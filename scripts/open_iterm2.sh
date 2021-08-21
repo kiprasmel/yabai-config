27661bb4b91efc8635b2c018fa9136ed3376b4f7
Kipras Melnikovas <kipras@kipras.org>
2021-08-21 03:18:44 +0300

    open_iterm2: refactor
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- scripts/open_iterm2.sh
+++ scripts/open_iterm2.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+open_app() {
+	app="/Applications/iTerm.app"
+    open -a "$app"
+}
+
 # Detects if iTerm2 is running
-if ! pgrep -f "iTerm" > /dev/null 2>&1; then
-    open -a "/Applications/iTerm.app"
+if ! pgrep -f "iTerm" &>/dev/null; then
+	open_app
 else
     # Create a new window
     script='tell application "iTerm2" to create window with default profile'
-    ! osascript -e "${script}" > /dev/null 2>&1 && {
+    ! osascript -e "${script}" &>/dev/null && {
         # Get pids for any app with "iTerm" and kill
         while IFS="" read -r pid; do
             kill -15 "${pid}"
         done < <(pgrep -f "iTerm")
-        open -a "/Applications/iTerm.app"
+		open_app
     }
 fi
